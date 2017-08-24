c6ea901bb28b3cc77443660defaa369ff5b3ebe5
N <dummy.42.user@gmail.com>
2017-08-24 18:26:16 +0300

    Installed dev modules. Created theme.

---
+++ themes/custom/school/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Javascript functionality for School Theme.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.DSSummaries = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
